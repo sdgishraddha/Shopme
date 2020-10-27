490f6e02e74768a3aae3be387679eb62d1b49b2d
Anand Mohan <im.anand.m@gmail.com>
2020-10-27 13:14:33 +0530

    Added User service class

---
+++ src/main/java/com/sportyshoesweb/service/UserService.java
@@ -0,0 +1,39 @@
+package com.sportyshoesweb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sportyshoesweb.model.User;
+import com.sportyshoesweb.repository.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> listAllUser() {
+		return userRepository.findAll();
+	}
+
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
+
+	public User getUserById(Integer userId) {
+
+		if (userRepository.findById(userId).isPresent()) {
+			return userRepository.findById(userId).get();
+		}
+		return null;
+	}
+
+	public void deleteUserById(Integer userId) {
+		userRepository.deleteById(userId);
+	}
+
+}
