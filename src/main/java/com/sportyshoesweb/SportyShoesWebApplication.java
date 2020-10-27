c9b711558011fa414f674c735e04e43e4072c84a
Anand Mohan <im.anand.m@gmail.com>
2020-10-27 12:07:48 +0530

    Sporty shoes project initialized

---
+++ src/main/java/com/sportyshoesweb/SportyShoesWebApplication.java
@@ -0,0 +1,13 @@
+package com.sportyshoesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportyshoesweb/SportyShoesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportyshoesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
