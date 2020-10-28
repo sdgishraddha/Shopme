fe66ce0efff565c62d07d6164e6a5b1488a359a8
Anand Mohan <im.anand.m@gmail.com>
2020-10-28 11:35:56 +0530

    Added user controller class

---
+++ src/main/java/com/sportyshoesweb/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.sportyshoesweb.controller;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sportyshoesweb.model.User;
+import com.sportyshoesweb.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@GetMapping("")
+	public List<User> listUsers() {
+		return userService.listAllUser();
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<User> getUserById(@PathVariable Integer userId) {
+		try {
+			User user = userService.getUserById(userId);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+
+		} catch (NoSuchElementException exp) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PostMapping("/")
+	public void addUser(@RequestBody User user) {
+		userService.saveUser(user);
+	}
+
+	@PutMapping("/{userId}")
+	public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Integer userId) {
+
+		if (userService.getUserById(userId) != null) {
+			user.setUserId(userId);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+	}
+
+	@DeleteMapping("/{userId}")
+	public void deleteUser(@PathVariable Integer userId) {
+		userService.deleteUserById(userId);
+	}
+
+}
