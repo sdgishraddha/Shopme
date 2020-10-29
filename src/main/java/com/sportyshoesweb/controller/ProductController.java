b9e277df5ca327e116d9cb7c0a5f3b782828cb44
Anand Mohan <im.anand.m@gmail.com>
2020-10-29 13:02:58 +0530

    Added Product Service and Controller class

---
+++ src/main/java/com/sportyshoesweb/controller/ProductController.java
@@ -0,0 +1,68 @@
+package com.sportyshoesweb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sportyshoesweb.model.Product;
+import com.sportyshoesweb.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+	@Autowired
+	ProductService productService;
+
+	@GetMapping("/{product_id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Integer product_id) {
+
+		if (productService.getProductById(product_id) != null)
+			return new ResponseEntity<>(productService.getProductById(product_id), HttpStatus.FOUND);
+
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+	}
+
+	@GetMapping("")
+	public List<Product> getAllProduct() {
+		return productService.listAllProduct();
+	}
+
+	@PostMapping("/")
+	public void saveProduct(@RequestBody Product product) {
+		productService.saveProduct(product);
+	}
+
+	@PutMapping("/{product_id")
+	public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable Integer product_id) {
+
+		if (productService.getProductById(product_id) != null) {
+
+			product.setProduct_id(product_id);
+
+			productService.saveProduct(product);
+
+			return new ResponseEntity<>(HttpStatus.OK);
+
+		}
+
+		return new ResponseEntity<>(product, HttpStatus.NOT_FOUND);
+	}
+
+	@DeleteMapping("/{product_id")
+	public void deleteProduct(@PathVariable Integer product_id) {
+		productService.deleteProductById(product_id);
+	}
+
+}
--- src/main/java/com/sportyshoesweb/service/ProductService.java
+++ src/main/java/com/sportyshoesweb/service/ProductService.java
@@ -1,5 +1,41 @@
 package com.sportyshoesweb.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sportyshoesweb.model.Product;
+import com.sportyshoesweb.repository.ProductRepository;
+
+@Service
+@Transactional
 public class ProductService {
+
+	@Autowired
+	ProductRepository productRepository;
+
+	public List<Product> listAllProduct() {
+		return productRepository.findAll();
+	}
+
+	public void saveProduct(Product product) {
+		productRepository.save(product);
+	}
+
+	public Product getProductById(Integer product_id) {
+
+		if (productRepository.findById(product_id).isPresent()) {
+			Product product = productRepository.findById(product_id).get();
+			return product;
+		}
+
+		return null;
+	}
+
+	public void deleteProductById(Integer product_id) {
+		productRepository.deleteById(product_id);
+	}
 
 }
