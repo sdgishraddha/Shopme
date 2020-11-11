53dc890a414ed9bba5d9fec72b090caf8bf3b9ea
Anand Mohan <im.anand.m@gmail.com>
2020-11-11 16:22:10 +0530

    Created HTML page to view all products

--- src/main/java/com/sportyshoesweb/controller/ProductController.java
+++ src/main/java/com/sportyshoesweb/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sportyshoesweb.model.Product;
 import com.sportyshoesweb.service.ProductService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/product")
 public class ProductController {
