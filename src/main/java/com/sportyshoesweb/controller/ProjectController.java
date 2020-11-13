807f45fd6ccab43088671f301ee81f8f85aba9dc
Anand Mohan <im.anand.m@gmail.com>
2020-11-13 15:23:17 +0530

    Added basic authentiation with in-memory credentials with Spring Security

---
+++ src/main/java/com/sportyshoesweb/controller/ProjectController.java
@@ -0,0 +1,46 @@
+package com.sportyshoesweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProjectController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public ModelAndView welcome() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login.html");
+		return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/admin")
+	public ModelAndView admin() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("adminpage.html");
+		return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/allproducts")
+	public ModelAndView allProducts() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("allproducts.html");
+		return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/allpurchase")
+	public ModelAndView allPurchase() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("allpurchase.html");
+		return modelAndView;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/allusers")
+	public ModelAndView allUsers() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("allusers.html");
+		return modelAndView;
+	}
+
+}
